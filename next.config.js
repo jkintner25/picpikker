6178c180b4194d5f8f447ebe3ce13964eb281484
Krista Strucke <kstrucke@gmail.com>
2023-03-04 18:57:15 -0800

    added uselocalstorage hook

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      unsplashAccessKey: 'JBELO4mJv3u5vFhPsCkJg1g7X50WsNKbqsl9_58ThiQ'
+  }
+}
+  module.exports = nextConfig
