61a77c9ab77046ad084c05663541ec7df1d3d330
Krista Strucke <kstrucke@gmail.com>
2023-03-04 20:26:00 -0800

    can store in local storage now

--- nextapp/src/components/ImageTile.js
+++ nextapp/src/components/ImageTile.js
@@ -1,12 +1,14 @@
-
+import { useLocalStorage } from '@/utils';
 
 const ImageTile = ({image}) => {
+
   return (
     <>
       <img
         className="transistion ease-in-out hover:scale-105 duration-200"
         alt={image.description || "image from unsplash"}
-        src={image.urls.small} />
+        src={image.urls.small}
+        onClick={() => useLocalStorage(image.id, image.links.download)} />
     </>
   );
 };
