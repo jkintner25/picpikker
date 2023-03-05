76a4fe943c6016dfc686c79ca63cd4fa1d2ad2fb
jkintner25 <jkintner25@gmail.com>
2023-03-04 23:52:46 -0600

    added onkeydown event listener to search input

--- nextapp/src/components/Search.js
+++ nextapp/src/components/Search.js
@@ -4,11 +4,16 @@ import { fetchImages, unsplashAPI } from "@/pages/api/unsplash";
 const Search = ({ setImages }) => {
   const [searchQuery, setSearchQuery] = useState('')
 
-  const submit = async (e) => {
-    e.preventDefault()
-    let images = await fetch(unsplashAPI.search.concat(searchQuery))
-    let data = await images.json()
-    setImages(data.results);
+
+  const submit = async (e, type) => {
+    if (type === "a") {
+      e.preventDefault()
+    }
+    if (searchQuery.length) {
+      let images = await fetch(unsplashAPI.search.concat(searchQuery))
+      let data = await images.json()
+      setImages(data.results);
+    }
   }
 
   const reset = async (e) => {
@@ -19,14 +24,19 @@ const Search = ({ setImages }) => {
     })
   }
 
+  const handleSearchInput = (e) => {
+    if (searchQuery.length && e.key === "Enter") submit(e, 'b')
+  }
+
   return (
     <div className="flex justify-center my-20">
       <input
         className="outline outline-offset-0 rounded-md mx-4 w-80 h-12 p-3"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyDown={handleSearchInput}
       />
-      <a href="" onClick={submit} className="relative inline-block text-lg group mx-2">
+      <a href="" onClick={(e) => submit(e, 'a')} className="relative inline-block text-lg group mx-2">
         <span className="relative z-10 block px-3.5 py-2 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-200 rounded-lg group-hover:text-white">
           <span className="absolute inset-0 w-full h-full px-3.5 py-2 rounded-lg bg-gray-50"></span>
           <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
