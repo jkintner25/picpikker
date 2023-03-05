6965faa901a1719113275400a82f3dca259f47d9
Krista Strucke <kstrucke@gmail.com>
2023-03-04 22:09:29 -0800

    progress on favorites button

--- nextapp/src/utils/index.js
+++ nextapp/src/utils/index.js
@@ -4,12 +4,30 @@ export function useLocalStorage(key, initialValue) {
 
     // Get from local storage by key
     const item = window.localStorage.getItem(key);
-    // Parse stored json or if none return initialValue
-    if (item) return item;
+    // If it's already in the storage, remove it from storage
+    if (item) localStorage.removeItem(key);
 
     // not in local storage:
 
     if (typeof window !== "undefined") {
         window.localStorage.setItem(key, JSON.stringify(initialValue));
     }
+};
+
+export function checkIfInStorage(imgId) {
+
+    // return true if in storage already
+    const item = window.localStorage.getItem(imgId);
+    return item ? true : false;
+    // return false if not
+}
+
+export function isEmpty(obj) {
+    // check if an object is empty
+    // return true if empty, false if not empty
+    for (let thing in obj) {
+        if (thing) return false;
+    }
+
+    return true;
 };
