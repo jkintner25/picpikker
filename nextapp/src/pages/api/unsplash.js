9d16c3acb9b3956800c17ad4d6a28de143340d04
jkintner25 <jkintner25@gmail.com>
2023-03-05 23:39:16 -0600

    updated env variables

--- nextapp/src/pages/api/unsplash.js
+++ nextapp/src/pages/api/unsplash.js
@@ -1,7 +1,7 @@
 export const unsplashAPI = {
-  photos: `https://api.unsplash.com/photos/?client_id=${process.env.unsplashAccessKey}&per_page=50&order_by=popular`,
-  randomImage: `https://api.unsplash.com/photos/random/?client_id=${process.env.unsplashAccessKey}`,
-  search: `https://api.unsplash.com/search/photos/?client_id=${process.env.unsplashAccessKey}&per_page=50&query=`
+  photos: `https://api.unsplash.com/photos/?client_id=${process.env.NEXT_PUBLIC_API}&per_page=50&order_by=popular`,
+  randomImage: `https://api.unsplash.com/photos/random/?client_id=${process.env.NEXT_PUBLIC_API}`,
+  search: `https://api.unsplash.com/search/photos/?client_id=${process.env.NEXT_PUBLIC_API}&per_page=50&query=`
 }
 
 export const fetchImages = async () => {
